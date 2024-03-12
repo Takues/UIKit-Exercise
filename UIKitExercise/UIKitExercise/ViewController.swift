cc0715871685713d6212edc2060f102869a8e779
Takuue <139447901+Takues@users.noreply.github.com>
2024-03-12 13:56:20 +0200

    Add files via upload

---
+++ UIKitExercise/UIKitExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitExercise
+//
+//  Created by Takudzwa Zindoga on 12/3/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
