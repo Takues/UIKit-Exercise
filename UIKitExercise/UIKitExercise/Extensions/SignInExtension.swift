b6f0e4cdbcfac8c91b6065276e737a9e02a4d847
Takues <takudzwazindoga07@gmail.com>
2024-03-13 10:04:49 +0200

    Created apps view controller and connected it to sign in

--- UIKitExercise/UIKitExercise/Extensions/SignInExtension.swift
+++ UIKitExercise/UIKitExercise/Extensions/SignInExtension.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    func showAlert(customTitle: String, customMessage: String ){
+        let alert = UIAlertController(title: customTitle, message: customMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
--- UIKitExercise/UIKitExercise/SignInViewController.swift
+++ UIKitExercise/UIKitExercise/SignInViewController.swift
@@ -9,6 +9,24 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var titleLabe: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBAction func signInAction(_ sender: Any) {
+        if let username = usernameTextField.text,
+            let password = passwordTextField.text,
+           !username.isEmpty && !password.isEmpty {
+
+            let appsTableViewControllerv = AppsViewController(nibName: "AppsViewController", bundle: Bundle.main)
+            navigationController?.pushViewController(appsTableViewControllerv, animated: true)
+
+
+        } else {
+            self.showAlert(customTitle: "Sign In Successful!", customMessage: "Click OK to continue")
+
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
